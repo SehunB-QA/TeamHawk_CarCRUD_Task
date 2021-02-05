101010098cbdb3d3a517cf9bd5b76756945dd3ea
CGuthrieQA <CGuthrie@QA.com>
2021-02-05 12:42:38 +0000

    initial front-end dev commit

--- script.js
+++ script.js
@@ -62,3 +62,79 @@ window.onload = function(){
    // garageID.addEventListener("click", deleteGarageByID)
 
 } 
+
+// query selectors
+
+    // buttons
+    const btnGetGarages = document.querySelector('#btnGetGarages');
+    const btnGetCars = document.querySelector('#btnGetCars');
+    
+      /////////////////////
+     // GET ALL GARAGES //
+    /////////////////////
+    
+    const getGarages = () => {
+    
+        fetch(`http://127.0.0.1:9092/garage/read`)
+            .then( (response) => {
+                (response.status !== 200) ? // OK
+                    console.error(`status ${response.status}`)
+                    : 
+                    response.json()
+                        .then( (data) => {
+                            console.info(data);
+                            // a_div.value = data.id; // garage id
+                            // a_div.value = data.name; // garage name
+    
+                            // // car stuff under here
+                            // a_div.value = data.cars;
+                            // for (car in data.cars){
+                            //     a_div.value = car.id;
+                            //     a_div.value = car.colour;
+                            //     a_div.value = car.doors;
+                            //     a_div.value = car.make;
+                            //     a_div.value = car.model;
+                            //     a_div.value = car.name;
+                            // };
+                        });
+            })
+            .catch( (err) => console.error(err) );  
+    
+    }
+    
+      //////////////////
+     // GET ALL CARS //
+    //////////////////
+    
+    const getCars = () => {
+    
+        fetch(`http://127.0.0.1:9092/car/read`)
+            .then( (response) => {
+                (response.status !== 200) ? // OK
+                    console.error(`status ${response.status}`)
+                    : 
+                    response.json()
+                        .then( (data) => {
+                            console.info(data);
+                            // a_div.value = data.id;
+                            // a_div.value = data.colour;
+                            // a_div.value = data.doors;
+                            // a_div.value = data.make;
+                            // a_div.value = data.model;
+                            // a_div.value = data.name;
+                        });
+            })
+            .catch( (err) => console.error(err) );  
+    
+    }
+    
+    
+    // pageload functions
+    getGarages();
+    
+    getCars();
+    
+    
+    // event listeners
+    btnGetGarages.addEventListener('click', getGarages);
+    btnGetCars.addEventListener('click', getCars);
